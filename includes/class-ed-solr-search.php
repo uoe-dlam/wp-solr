d6d3d201865a665fb0cff18b083e31ba40b1d4e7
Richard Lawson <richard.lawson@ed.ac.uk>
2019-08-13 15:27:31 +0100

    update search query

--- includes/class-ed-solr-search.php
+++ includes/class-ed-solr-search.php
@@ -99,9 +99,7 @@ class Ed_Solr_Search {
 	 * @return string
 	 */
 	private function get_query_string() {
-		$query_string  = 'blogId:(' . implode( ' OR ', $this->blog_ids ) . ')';
-		$query_string .= ' AND postTitle:(' . $this->keywords . ')';
-		$query_string .= ' OR postContent:(' . $this->keywords . ')';
+		$query_string  = 'blogId:(' . implode( ' OR ', $this->blog_ids ) . ') AND (postTitle:(' . $this->keywords . ') OR postContent:(' . $this->keywords . '))';
 
 		if ( ! $this->show_ease ) {
 			$query_string .= ' AND easeOnly:0';
