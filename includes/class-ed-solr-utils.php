b6d38069f8f4c7bbd74a8c302b951b5cd8bb212e
Edtg <edward.green@ed.ac.uk>
2026-07-22 18:19:33 +0100

    Added a utility class with a function to get the posts count of a user

---
+++ includes/class-ed-solr-utils.php
@@ -0,0 +1,51 @@
+<?php
+
+use Solarium\Core\Client\Adapter\Curl;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class Ed_Solr_Utils
+{
+	public function __construct()
+	{
+		$this->solr_client = $this->get_solr_client();
+	}
+
+	/**
+	 * Get solarium client
+	 *
+	 * @return Solarium\Client
+	 */
+	private function get_solr_client() {
+		$client = new Solarium\Client(
+			new Curl(),
+			new EventDispatcher(),
+			[
+				'endpoint' => [
+					'localhost' => [
+						'host'     => get_site_option( 'solr-host' ),
+						'port'     => get_site_option( 'solr-port' ),
+						'path'     => get_site_option( 'solr-path' ),
+						'core'     => get_site_option( 'solr-core' ),
+						'username' => get_site_option( 'solr-username' ),
+						'password' => get_site_option( 'solr-password' ),
+					],
+				],
+			]
+		);
+
+		$client->getPlugin( 'postbigrequest' );
+
+		return $client;
+	}
+
+	public function getUserPostsCount(string $user_id)
+	{
+		$query = $this->solr_client->createSelect();
+		$query->setQuery('author:(' . $user_id . ')');
+		$query->setRows(0);
+
+		$resultSet = $this->solr_client->select($query);
+
+		return $resultSet->getNumFound();
+	}
+}
